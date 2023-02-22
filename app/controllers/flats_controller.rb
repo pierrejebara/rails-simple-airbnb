1073b6fad0a33a8947a4a0711d455eee0971a553
pierre jebara <pierrehenrijebara@gmail.com>
2023-02-22 16:56:14 +0100

    done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,31 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :price_per_night, :number_of_guests, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Nice House in West Bloomingfield',
+  address: '25 Clifton Gardens London W9 4DT',
+  description: 'Huge apartment with indoor pool',
+  price_per_night: 200,
+  number_of_guests: 5,
+)
